83f4337cdbbfc18502c4a84f82b03413bb6a04f5
suvidhak96 <suvidhak96@gmail.com>
2024-01-18 16:18:34 +0530

    image to text converter  using tesseract ocr

---
+++ ocr/index.js
@@ -0,0 +1,46 @@
+// text format //
+const express=require('express');
+const multer=require('multer');
+const tesseract = require("node-tesseract-ocr")
+const path=require('path');
+const app= express();
+
+app.use(express.static(path.join(__dirname+'/uploads')));
+
+app.set('view engine','ejs');
+var storage=multer.diskStorage({
+    destination:function(req,file,cb){
+        cb(null,'uploads')
+    },
+    filename:function(req,file,cb){
+        // cb(null, file.fieldname+'-'+Date.now()+path.extname(file.originalname));
+        cb(null,file.originalname)
+    },
+})
+const upload=multer({storage:storage})
+
+app.get('/',(req,res)=>{
+    res.render('index',{data:''})
+})
+
+app.post('/extracttextfromimage',upload.single('file'),(req,res)=>{
+    console.log("path is", req.file.path)
+    const config = {
+        lang: "eng",
+        oem: 1,
+        psm: 3,
+      }
+      
+      tesseract.recognize(req.file.path, config).then((text) => {
+          console.log("Result:", text)
+            res.render('index',{data:text})
+        })
+        .catch((error) => {
+          console.log(error.message)
+        })
+})
+app.listen(5500,()=>{
+    console.log("server is running")
+})
+
+
